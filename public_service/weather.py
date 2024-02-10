a24dedf23c6f8bf2713d070932c6b070cd06628f
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2024-02-10 11:46:54 +0200

    Added documentation for socket project, finished API project

--- public_service/weather.py
+++ public_service/weather.py
@@ -4,6 +4,7 @@ from geolocation import Geolocation
 class WeatherClient:
     def __init__(self):
         self.coords=self.get_coords()
+        self.weather=self.get_weather()
 
     def get_coords(self):
         geolocator = Geolocation("http://ip-api.com/json/?fields=lat,lon")
@@ -12,10 +13,21 @@ class WeatherClient:
     def get_weather(self):
         lat=self.coords['lat']
         lon=self.coords['lon']
-        url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m"
+        url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,rain,showers,snowfall,snow_depth,wind_speed_10m,wind_direction_10m,soil_temperature_0cm,soil_moisture_0_to_1cm"
         response = requests.get(url)
         return response.json()['current']
 
+    def display_results(self):
+        print(f"Temperatura: {self.weather['temperature_2m']}")
+        print(f"Sansa de ploaie: {self.weather['rain']}")
+        print(f"Sansa de ninsoare: {self.weather['snowfall']}")
+        if self.weather['snowfall']>0:
+            print(f"Adancimea zapezii: {self.weather['snow_depth']}")
+        print(f"Viteza vantului: {self.weather['wind_speed_10m']}")
+        print(f"Directia vantului: {self.weather['wind_direction_10m']}")
+        print(f"Temperatura pamantului: {self.weather['soil_temperature_0cm']}")
+        print(f"Umiditatea pamantului: {self.weather['soil_moisture_0_to_1cm']}")
+
 if __name__ == "__main__":
     weather = WeatherClient()
-    print(weather.get_weather())
+    weather.display_results()
